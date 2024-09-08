4cb479e4c6d76aa1d0f15e107b0436e7114ddeeb
balmoorthy <tamilanbala72@gmail.com>
2024-09-08 13:22:35 +0400

    update

--- app/account/page.js
+++ app/account/page.js
@@ -1,3 +1,11 @@
+export const metadata = {
+  title: "Account",
+};
+
 export default function Page() {
-  return <h3>Account page</h3>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, Bala
+    </h2>
+  );
 }
